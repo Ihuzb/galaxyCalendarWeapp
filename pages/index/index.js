0c29ca96234c95ec03beb60da6b3f1df7b5eac34
yintianyu <yintianyu@ywys.com>
2022-01-12 21:11:13 +0800

    #0112 完整版提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,20 +1,62 @@
 // index.js
 // 获取应用实例
 const app = getApp()
-
+const chooseLocation = requirePlugin('chooseLocation');
+const QQMapWX = require('./qqmap-wx-jssdk.min');
+import galaxyTime from 'galaxy-time'
+let {
+  formattingTime,
+  formattingMoonPhase,
+  formattingGalaxyAzimuth
+} = require('../../public/index')
+// 实例化API核心类
+const qqmapsdk = new QQMapWX({
+  key: '3NDBZ-4JWKO-KKGWF-SJKGH-IA4LV-TOB73' // 必填
+});
 Page({
   data: {
     motto: 'Hello World',
+    moonPhase: '🌕',
+    direction: '', // 方向
     userInfo: {},
+    location: {},
+    galaxyInfo: {},
+    activeNames: ['1'],
+    maxDate: new Date(2023, 0, 31).getTime(),
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
-    formatterInfo(day){
-      day.bottomInfo = '⭐️⭐️⭐️';
-      // day.bottomInfo = '⭐️🪐🌌🪰⭐️🌟✨💫🌕🌖🌗🌘🌑🌒🌓🌔';
-      return day;
-    }
+  },
+  onChange(event) {
+    this.setData({
+      activeNames: event.detail,
+    });
+  },
+  onConfirm(event) {
+    let {
+      latitude,
+      longitude
+    } = this.data.location;
+    this.galaxy_time(event.detail, latitude, longitude)
+  },
+  setFormatterInfo({
+    latitude = '',
+    longitude = ''
+  }) {
+    const _this = this;
+    _this.setData({
+      formatterInfo: function (day) {
+        let times = formattingTime(day.date);
+        let info = galaxyTime.getTrueGalaxyTimes(times, latitude, longitude);
+        if (info.code == 1) {
+          let hours = Number(info.hours);
+          day.bottomInfo = hours <= 2 ? '⭐️' : hours <= 4 ? '⭐️⭐️' : '⭐️⭐⭐️'
+        }
+        // day.bottomInfo = '⭐️🪐🌌🪰⭐️🌟🌌💫🌕🌖🌗🌘🌑🌒🌓🌔';
+        return day;
+      }
+    })
   },
   // 事件处理函数
   bindViewTap() {
@@ -22,19 +64,128 @@ Page({
       url: '../logs/logs'
     })
   },
+  startLocation() {
+    let _this = this;
+    wx.getLocation({
+      type: 'wgs84',
+      success(res) {
+        let {
+          latitude,
+          longitude
+        } = res;
+        // 设置日历自定义文案
+        _this.setFormatterInfo({
+          latitude,
+          longitude
+        });
+        let location = {
+          latitude,
+          longitude
+        };
+        qqmapsdk.reverseGeocoder({
+          location: {
+            latitude,
+            longitude
+          },
+          success: function (res) { //成功后的回调
+            const data = res.result;
+            let {
+              formatted_addresses,
+              address_component
+            } = data;
+            location["name"] = formatted_addresses.recommend;
+            location["district"] = address_component.district;
+            location["city"] = address_component.city;
+            location["province"] = address_component.province;
+            _this.setData({
+              location,
+            })
+          }
+        })
+      }
+    })
+  },
   onLoad() {
+    let _this = this;
+    wx.authorize({
+      scope: 'scope.userLocation',
+      success() {
+        _this.startLocation()
+      }
+    })
     if (wx.getUserProfile) {
       this.setData({
         canIUseGetUserProfile: true
       })
     }
   },
+  galaxy_time(date, lat, lon) {
+    let galaxyInfo = this.data.galaxyInfo;
+    let times = formattingTime(date);
+    // 银河方位角数据
+    let GalaxyPosition = galaxyTime.getGalaxyPosition(date, lat, lon);
+    // 银河可见时间数据
+    let TrueGalaxyTimes = galaxyTime.getTrueGalaxyTimes(times, lat, lon);
+    // 月亮数据 月相
+    let MoonPosition = galaxyTime.getMoonIllumination(date, lat, lon);
+    // 月亮时间
+    let MoonTimes = galaxyTime.getMoonTimes(date, lat, lon);
+    // 日出日落
+    let Times = galaxyTime.getTimes(date, lat, lon);
+    let {
+      phase
+    } = MoonPosition;
+    let {
+      azimuth
+    } = GalaxyPosition;
+    galaxyInfo["GalaxyPosition"] = GalaxyPosition;
+    galaxyInfo["TrueGalaxyTimes"] = TrueGalaxyTimes;
+    galaxyInfo["MoonTimes"] = MoonTimes;
+    galaxyInfo["Times"] = Times;
+    this.setData({
+      galaxyInfo,
+      moonPhase: formattingMoonPhase(phase),
+      direction: formattingGalaxyAzimuth(azimuth)
+    })
+    console.log(galaxyInfo);
+  },
+  onShow() {
+    let _this = this;
+    const location = chooseLocation.getLocation(); // 如果点击确认选点按钮，则返回选点结果对象，否则返回null
+    if (location) {
+      let {
+        latitude,
+        longitude
+      } = location;
+      // 设置日历自定义文案
+      _this.setFormatterInfo({
+        latitude,
+        longitude
+      });
+      this.setData({
+        location
+      })
+    }
+
+  },
+  onUnload() {
+    // 页面卸载时设置插件选点数据为null，防止再次进入页面，geLocation返回的是上次选点结果
+    chooseLocation.setLocation(null);
+  },
+  showMap() {
+    const key = '3NDBZ-4JWKO-KKGWF-SJKGH-IA4LV-TOB73'; //使用在腾讯位置服务申请的key
+    const referer = '逸生活YI'; //调用插件的app的名称
+    const category = '生活服务,娱乐休闲';
+
+    wx.navigateTo({
+      url: `plugin://chooseLocation/index?key=${key}&referer=${referer}&category=${category}`
+    });
+  },
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
-        console.log(res)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
@@ -44,7 +195,6 @@ Page({
   },
   getUserInfo(e) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
---
+++ public/index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    formattingTime: (dates = '') => {
+        let days = new Date(dates);
+        let years = days.getFullYear(),
+            month = (days.getMonth() + 1).toString(),
+            date = (days.getDate()).toString();
+        let times = `${years}-${month.padStart(2,'0')}-${date.padStart(2,'0')}`;
+        return times
+    },
+    formattingMoonPhase: (phase = 0) => {
+        return phase >= 0.75 ? '🌗' : phase >= 0.5 ? '🌕' : phase >= 0.25 ? '🌓' : '🌑'
+    },
+    formattingGalaxyAzimuth: (azimuth = 0) => {
+        let trueAzimuth = azimuth + 180;
+        switch (true) {
+            case trueAzimuth >= 360 || trueAzimuth <= 0:
+                return '北';
+                break;
+            case trueAzimuth >= 315:
+                return '西北';
+                break;
+            case trueAzimuth >= 270:
+                return '西';
+                break;
+            case trueAzimuth >= 225:
+                return '西南';
+                break;
+            case trueAzimuth >= 180:
+                return '南';
+                break;
+            case trueAzimuth >= 135:
+                return '东南';
+                break;
+            case trueAzimuth >= 90:
+                return '东';
+                break;
+            case trueAzimuth >= 45:
+                return '东北';
+                break;
+        }
+    },
+}
